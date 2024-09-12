b86a6da0affe1ebe3bff6b98af082931b4d90929
hans <joshuaehans@gmail.com>
2024-09-12 11:43:31 -0400

    heat index and concatenation

--- 00-weather-algorithms/period-5/Weather.java
+++ 00-weather-algorithms/period-5/Weather.java
@@ -46,16 +46,27 @@ public class Weather {
         } // end catch
 
         // display arrays to see all values
-        System.out.println(Arrays.toString(temps23));
-        System.out.println();
-        System.out.println(Arrays.toString(humidity23));
-        System.out.println();
-        System.out.println(Arrays.toString(temps24));
-        System.out.println();
-        System.out.println(Arrays.toString(humidity24));
+        // System.out.println(Arrays.toString(temps23));
+        // System.out.println();
+        // System.out.println(Arrays.toString(humidity23));
+        // System.out.println();
+        // System.out.println(Arrays.toString(temps24));
+        // System.out.println();
+        // System.out.println(Arrays.toString(humidity24));
 
-        System.out.println(getSum(temps23));
-        System.out.println(getSum(temps24));
+        // System.out.println(getSum(temps23));
+        // System.out.println(getSum(temps24));
+
+        int temp = 90;
+        int hum = 60;
+        System.out.println("when the temperature is " + temp + " and the humidity is " 
+                            + hum + " the real feel is: " + calculateHeatIndex(temp, hum));
+
+        temp = 100;
+        hum = 40;
+        System.out.println("when the temperature is " + temp + " and the humidity is " 
+                            + hum + " the real feel is: " + calculateHeatIndex(temp, hum));
+                    
     } // close main method
 
     public static int getSum(int[] nums) {
@@ -71,5 +82,27 @@ public class Weather {
         // STEP 3:
         return total;
     } // end getSum method
+
+    // code from Derek
+    public static int calculateHeatIndex(int t, int h) {
+        // making variables for constants
+        double c1 = -42.379;
+        double c2 = 2.04901523;
+        double c3 = 10.14333127;
+        double c4 = .22475541;
+        double c5 = .00683783;
+        double c6 = .05481717;
+        double c7 = .00122874;
+        double c8 = .00085282;
+        double c9 = .00000199;
+
+        double heatIndex = c1 + (c2 * t) + (c3 * h) - (c4 * t * h) - 
+                                    (c5 * t * t) - (c6 * h * h) + (c7 * t * t * h) + 
+                                    (c8 * t * h * h) - (c9 * t * t * h * h);
+
+        // System.out.println("The real feel temperature is: " + heatIndex);
+        
+        return (int)(heatIndex + 0.5);
+    }
 
 } // close main class
