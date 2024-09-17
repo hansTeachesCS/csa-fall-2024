15eacfa311ad2085f763660fdc1bd99a574f8b80
hans <joshuaehans@gmail.com>
2024-09-17 09:55:42 -0400

    can accept input

--- 02-user-input/period-3/Average.java
+++ 02-user-input/period-3/Average.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class Average {
     public static void main(String[] args) {
+        // STEP 1: create Scanner object
+        Scanner s = new Scanner(System.in);
+
+        // STEP 2: display a prompt on the screen
+        System.out.print("enter your first test score: ");
+
+        // STEP 3: accept and store the input
+        int score1 = s.nextInt();
+
+        System.out.println(score1);
 
     } // end main method
 
     /*
      * N: calculateAverage
      * P: find the average of 3 numbers
-     * I: 3 doubles
+     * I: 3 ints
      * R: a double
      */
-    public static double calculateAverage(double a, double b, double c) {
+    public static double calculateAverage(int a, int b, int c) {
          
         return -1;
     } // end calculateAverage method
