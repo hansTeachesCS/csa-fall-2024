1e408e34f18c9e7de1d47b97959371287988e5e5
hans <joshuaehans@gmail.com>
2024-09-17 09:41:30 -0400

    starter code

---
+++ 02-user-input/period-3/Average.java
@@ -0,0 +1,18 @@
+public class Average {
+    public static void main(String[] args) {
+
+    } // end main method
+
+    /*
+     * N: calculateAverage
+     * P: find the average of 3 numbers
+     * I: 3 doubles
+     * R: a double
+     */
+    public static double calculateAverage(double a, double b, double c) {
+         
+        return -1;
+    } // end calculateAverage method
+
+
+} // end Average class
