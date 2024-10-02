93e15da2a79bb2543ccbf9e6eb6a53557ee8be64
hans <joshuaehans@gmail.com>
2024-10-02 09:55:33 -0400

    middleThree complete

--- 03-strings/period-3/Strings.java
+++ 03-strings/period-3/Strings.java
@@ -8,8 +8,25 @@ public class Strings {
         String str = s.nextLine();
 
         // send the String to a method to test
-
+        System.out.println(middleThree(str));
 
         s.close();
+    }
+
+    public static String middleThree(String str) {
+        // determine the length of str
+        int length = str.length();
+
+        // reduce length by dividing by two
+        length /= 2;
+
+        // subtract by 1
+        int from = length - 1;
+
+        // add two
+        int to = length + 2;
+
+        // return
+        return str.substring(from, to);
     }
 }
