2b8dfb471c72dc3de9785d2054e6eb5dfc8e6096
hans <joshuaehans@gmail.com>
2024-10-02 09:30:10 -0400

    starter

---
+++ 03-strings/period-3/Strings.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // get user input
+        String str = s.nextLine();
+
+        // send the String to a method to test
+
+
+        s.close();
+    }
+}
